2efa1e9bce39bc4eb3c38bde1b0185f62c191499
Chrix Eledu <chrixeledu@students.teau.ac.ke>
2026-07-09 01:00:03 +0300

    Added Field description to Telemetry Payload schema for improved documentation

--- app/schemas/telemetry.py
+++ app/schemas/telemetry.py
@@ -1,17 +1,31 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 from typing import Any
 from datetime import datetime
 
 class TelemetryPayload(BaseModel):
-    model_config = {"extra": "forbid"}
-    node_id: str = Field(...)
-    timestamp: datetime = Field(default_factory=datetime.now)
-    metric_type: str = Field(..., description="Industrial Node Metric e.g. 'power_quality', 'sensor_freq', thermal_factor, 'system_alert', 'anomaly'")
+    model_config = ConfigDict(extra="forbid")
+    node_id: str = Field(
+        ...,
+        description="Unique identifire for the edge node asset"
+    )
+    timestamp: datetime = Field(
+        default_factory=datetime.now,
+        description="UTC isolation timestamp"
+    )
+    metric_type: str = Field(
+        ...,
+        description="Node Metric e.g. 'power_quality', 'time-series data'"
+    )
     data_summary: str = Field(
         ..., 
-        description="Unstructured textual data of the metrics for embedding")
+        description="Unstructured textual data of the metrics for" \
+        "embedding calculations"
+    )
     structured_metrics: dict[str, Any] = Field(
         default_factory=dict,
         description="Key-value store for numerical metrics"
     )
-    status_code: int
+    status_code: int = Field(
+        ..., 
+        description="Operational status code from the hardware node loop"
+    )
